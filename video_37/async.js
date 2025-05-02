f837f8c2d4d21171e8d39d9cea049f62e2f13590
ubaidalishah <ubaidalishah18@gmail.com>
2025-05-02 17:29:16 +0500

    added-work-related-async

---
+++ video_37/async.js
@@ -0,0 +1,7 @@
+// by default javascript is synchronous
+//by default single threaded language
+// blocking code vs non blocking code 
+// blocking code block the flow of the program -- e.g read file sync -- access only nodejs environment
+// non blocking code does not block the flow of the program -- read file async
+// diagram of the async functionality of js can be found in the given pdf in this folder
+
